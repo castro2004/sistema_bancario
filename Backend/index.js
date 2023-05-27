2de855d583e3f236ca820c20991abb91dba5717e
jcastro2021416 <jcastro-2021416@kinal.edu.gt>
2023-05-27 13:16:27 -0600

    Se agrego la conexion

---
+++ Backend/index.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const express = require('express');
+const app = express();
+const {connection} = require("./src/database/connection");
+require('dotenv').config();
+const port = process.env.PORT;
+connection();
+
+//app.use(express.urlencoded({extended: false}));
+
+//app.use(express.json());
+
+//app.use('/api', routes, routesU)
+
+app.listen(port, ()=>{
+    console.log(`Servidor corriendo en el puerto ${port}`);
+})
