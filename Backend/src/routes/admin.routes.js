5a55f87069f29082f2ff80259d54210aa0a6efe8
jcastro2021416 <jcastro-2021416@kinal.edu.gt>
2023-06-03 13:02:30 -0600

    Se agregaron las rutas correspondientes, dependendiendo de cada rol y funcion

--- Backend/src/routes/admin.routes.js
+++ Backend/src/routes/admin.routes.js
@@ -1,8 +1,8 @@
 'use strict'
 
 const {Router} = require('express');
-const { createAdmin, searchAdmin, deleteAdmin, updateAdmin, loginAdmin, getAccountsByTransactionCount, viewDataAdmin } = require("../controller/adminController");
-// const adminController = require('../controllers/adminController');
+const { createAdmin, deleteAdmin, updateAdmin, loginAdmin, getAccountsByTransactionCount, viewDataAdmin } = require("../controller/adminController");
+const { createUser, deleteUser } = require("../controller/userController")
 const api = Router();
 api.post('/create-admin', createAdmin);
 api.get('/viewData-admin', viewDataAdmin);
@@ -10,5 +10,7 @@ api.delete('/delete-admin/:id', deleteAdmin);
 api.put('/update-admin/:id', updateAdmin);
 api.post('/login-admin', loginAdmin);
 api.get('/admin/accounts', getAccountsByTransactionCount)
+api.post('/create-user', createUser);
+api.delete('/delete-user/:id', deleteUser);
 
 module.exports = api;
--- Backend/src/routes/user.routes.js
+++ Backend/src/routes/user.routes.js
@@ -3,11 +3,11 @@
 const {Router} = require('express');
 const {createUser, deleteUser, updateUser, loginUser, viewUserData, viewBalance, historyTransaction} = require("../controller/userController");
 const { createFavorite, readFavorite, updateFavorite, deleteFavorite } = require('../controller/favoriteController')
+const { createTransferencias } = require('../controller/transferenciaController')
+
 
 const api = Router();
-api.post('/create-user', createUser);
 api.get('/viewUserData-user/', viewUserData);
-api.delete('/delete-user/:id', deleteUser);
 api.put('/update-user/:id', updateUser);
 api.post('/login-user', loginUser);
 api.get('/viewBalance-user', viewBalance )
@@ -16,5 +16,6 @@ api.post('/create-favorite', createFavorite)
 api.get('/read-favorite', readFavorite);
 api.put('/update-favorite', updateFavorite);
 api.delete('delete-favorite', deleteFavorite);
+api.post('/create-transfencias', createTransferencias);
 
 module.exports = api;
