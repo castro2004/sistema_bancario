9e8aeebf2ddf86c3128912782973665f70dbe6f5
jcastro2021416 <jcastro-2021416@kinal.edu.gt>
2023-06-03 03:58:57 -0600

    Se agrego CRUD para los favoritos del usuario

--- Backend/src/routes/user.routes.js
+++ Backend/src/routes/user.routes.js
@@ -2,6 +2,7 @@
 
 const {Router} = require('express');
 const {createUser, deleteUser, updateUser, loginUser, viewUserData, viewBalance, historyTransaction} = require("../controller/userController");
+const { createFavorite, readFavorite, updateFavorite, deleteFavorite } = require('../controller/favoriteController')
 
 const api = Router();
 api.post('/create-user', createUser);
@@ -11,5 +12,9 @@ api.put('/update-user/:id', updateUser);
 api.post('/login-user', loginUser);
 api.get('/viewBalance-user', viewBalance )
 api.get('/historyTransaction-user/:id', historyTransaction)
+api.post('/create-favorite', createFavorite)
+api.get('/read-favorite', readFavorite);
+api.put('/update-favorite', updateFavorite);
+api.delete('delete-favorite', deleteFavorite);
 
 module.exports = api;
