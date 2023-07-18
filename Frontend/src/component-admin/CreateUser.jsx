f030206fd0b9e600600ae3e78e6654b7761ae515
jcastro2021416 <jcastro-2021416@kinal.edu.gt>
2023-07-17 22:43:11 -0600

    Ayer

--- Frontend/src/component-admin/CreateUser.jsx
+++ Frontend/src/component-admin/CreateUser.jsx
@@ -274,7 +274,7 @@ const CreateUser = () => {
               <ul/>
               <div style={{ display: 'flex', justifyContent: 'center', marginTop: '20px' }}>
                 <button
-                onClick={mostrarAlerta}
+                onClick={()=>mostrarAlerta()}
                   type="submit"
                   style={{
                     backgroundColor: '#c0a57d',
@@ -297,6 +297,40 @@ const CreateUser = () => {
 };
 
 export default CreateUser;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
