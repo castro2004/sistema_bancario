bcf3e982cda6825bea1c9f4daec5eb8ad4c8264d
jcastro2021416 <jcastro-2021416@kinal.edu.gt>
2023-07-30 23:46:05 -0600

    Eliminacion de archivos

--- Backend/src/routes/transferencia.routes.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-const {Router} = require('express');
-const { createTransferencias } = require("../controller/transferenciaController");
-
-const api = Router();
-api.post('/create-transfencias', createTransferencias);
-
-module.exports = api;
--- Frontend/src/Components-user/UpdateFavorite.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const UpdateFavorite = () => {
-  return (
-    <div>UpdateFavorite</div>
-  )
-}
-
-export default UpdateFavorite
